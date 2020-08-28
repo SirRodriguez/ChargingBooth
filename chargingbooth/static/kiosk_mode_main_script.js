ea588e211d2699fd1d37591955c277dfd5818a69
Christian Rodriguez <christianrodriguez795@yahoo.com>
2020-08-28 14:37:09 -0700

    Moved the battery notification icon down in the session

--- chargingbooth/static/kiosk_mode_main_script.js
+++ chargingbooth/static/kiosk_mode_main_script.js
@@ -50,6 +50,27 @@ function showSlides(n, timeout) {
 
 // Sessions
 
+// Returns True if there are sessions running, false othersiwse
+// Only works when there is only one session allowed at a time!
+function sessions_active(){
+  var session_time_remains = document.getElementsByClassName("time_remaining");
+
+
+  if( session_time_remains.length > 0 ){
+    // Check if the sessions on screen have completed
+    if( session_time_remains[0].innerHTML == "<mark><b>Session Completed!</b></mark>" ){
+      return false;
+
+    // If they are not completed
+    }else{
+      return true;
+    }
+
+  }else{
+    return false;
+  }
+}
+
 function make_time_string(hour, minute, second){
   return ( hour > 9 ? hour.toString() : "0" + hour.toString() ) + ":"
           + ( minute > 9 ? minute.toString() : "0" + minute.toString() ) + ":"
@@ -66,20 +87,10 @@ function count_sessions(){
   var start_sess_btn = document.getElementById("Btn");
   var sess_in_prog = document.getElementById("progBtn");
 
-  if( session_time_remains.length > 0 ){
-    // Check if the sessions on screen have completed
-    if( session_time_remains[0].innerHTML == "<mark><b>Session Completed!</b></mark>" ){
-      // Show the Click To Start A Session
-      start_sess_btn.style.display = "block";
-      sess_in_prog.style.display = "none";
-
-    // If they are not completed
-    }else{
-      // Block the Click To Start a Session
-      start_sess_btn.style.display = "none";
-      sess_in_prog.style.display = "block";
-    }
-
+  if(sessions_active()){
+    // Block the Click To Start a Session
+    start_sess_btn.style.display = "none";
+    sess_in_prog.style.display = "block";
   }else{
     // Show the Click To Start A Session
     start_sess_btn.style.display = "block";
@@ -201,21 +212,32 @@ function count_sessions(){
 
 
 function chargebattery() {
-  var a;
-  a = document.getElementById("battery");
-  a.innerHTML = "&#xf244;";
-  setTimeout(function () {
-      a.innerHTML = "&#xf243;";
-    }, 1000);
-  setTimeout(function () {
-      a.innerHTML = "&#xf242;";
-    }, 2000);
-  setTimeout(function () {
-      a.innerHTML = "&#xf241;";
-    }, 3000);
-  setTimeout(function () {
-      a.innerHTML = "&#xf240;";
-    }, 4000);
+  // Check to see if the session is still active
+  // Otherwise, Pause the battery motion
+
+  if(sessions_active()){
+    var a;
+    a = document.getElementById("battery");
+    a.innerHTML = "&#xf244;";
+
+    setTimeout(function () {
+        a.innerHTML = "&#xf243;";
+      }, 1000);
+
+    setTimeout(function () {
+        a.innerHTML = "&#xf242;";
+      }, 2000);
+
+    setTimeout(function () {
+        a.innerHTML = "&#xf241;";
+      }, 3000);
+
+    setTimeout(function () {
+        a.innerHTML = "&#xf240;";
+      }, 4000);
+  }
+
+  setTimeout(chargebattery, 5000);
 }
+
 chargebattery();
-setInterval(chargebattery, 5000);
