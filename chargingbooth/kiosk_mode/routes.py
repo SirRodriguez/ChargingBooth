68aac651ad27885dc632bf68b080cdbb7f571830
Christian Rodriguez <christianrodriguez795@yahoo.com>
2020-09-12 18:01:12 -0700

    Removed all unnecessary files, forms, and routes in kiosk mode

--- chargingbooth/kiosk_mode/routes.py
+++ chargingbooth/kiosk_mode/routes.py
@@ -6,7 +6,7 @@ from flask import render_template, Blueprint, redirect, url_for, flash, request
 from flask_login import current_user, logout_user
 from chargingbooth import db, bcrypt, current_sessions
 from chargingbooth.models import Session, Settings
-from chargingbooth.kiosk_mode.forms import DataForm, RandomDataForm, SessionForm, ConfirmSessionForm
+# from chargingbooth.kiosk_mode.forms import
 from chargingbooth.kiosk_mode.utils import start_route, get_offset_dates_initiated, get_offset_dates_end
 from chargingbooth.models import PFI
 
@@ -65,98 +65,3 @@ def make_session():
 		flash('Session Added Successfully! You may start charging now.')
 
 	return redirect(url_for('kiosk_mode.home'))
-
-@kiosk_mode.route("/kiosk_mode/simulated_session")
-def simulated_session():
-	start_route()
-
-	setting = Settings.query.first()
-	return render_template('kiosk_mode/simulated_session.html', title='Simulate', setting=setting)
-
-@kiosk_mode.route("/kiosk_mode/session", methods=['GET', 'POST'])
-def session():
-	start_route()
-
-	setting = Settings.query.first()
-	form = SessionForm()
-	if form.validate_on_submit():
-		amount_paid = setting.cents_per_second * setting.charge_time * form.num_of_sessions.data
-		location = "current_location"
-		port = "current_port"
-		increment_size = setting.charge_time
-		increments = form.num_of_sessions.data
-		current_sessions.add_session(amount_paid, location, port, increment_size, increments)
-		flash('You have created a session and you may now start charging.', 'success')
-		return redirect(url_for('kiosk_mode.home'))
-
-	return render_template('kiosk_mode/session.html', title='Session', form=form, setting=setting)
-
-@kiosk_mode.route("/kiosk_mode/add_session", methods=['GET', 'POST'])
-def add_session():
-	start_route()
-
-	form = DataForm()
-	if form.validate_on_submit():
-		session = Session(duration=form.duration.data, power_used=form.power_used.data, 
-							amount_paid=form.amount_paid.data, location=form.location.data, port=form.port.data, 
-							increment_size=form.increment_size.data, increments=form.increments.data)
-		db.session.add(session)
-		db.session.commit()
-		flash('Session Added!', 'success')
-		return redirect(url_for('kiosk_mode.add_session'))
-	return render_template('kiosk_mode/add_session.html', title='Add', form=form)
-
-@kiosk_mode.route("/kiosk_mode/add_rand_session", methods=['GET', 'POST'])
-def add_rand_session():
-	start_route()
-
-	form = RandomDataForm()
-	if form.validate_on_submit():
-
-		if form.min_duration.data > form.max_duration.data:
-			flash('Min Duration must be less than Max Duration', 'danger')
-			return render_template('kiosk_mode/add_rand_session.html', form=form)
-
-		elif form.min_power_used.data > form.max_power_used.data:
-			flash('Min Power Used must be less than Max Power Used', 'danger')
-			return render_template('kiosk_mode/add_rand_session.html', form=form)
-
-		elif form.min_amount_paid.data > form.max_amount_paid.data:
-			flash('Min Cents must be less than Max Cents', 'danger')
-			return render_template('kiosk_mode/add_rand_session.html', form=form)
-
-		elif form.min_port.data > form.max_port.data:
-			flash('Min Port must be less than Max Port', 'danger')
-			return render_template('kiosk_mode/add_rand_session.html', form=form)
-
-		elif form.min_increment_size.data > form.max_increment_size.data:
-			flash('Min Increment Size must be less than Max Increment Size', 'danger')
-			return render_template('kiosk_mode/add_rand_session.html', form=form)
-
-		else:
-			for s in range(form.num_sessions.data):
-				duration = random.randint(form.min_duration.data, form.max_duration.data)
-				power_used = random.uniform(form.min_power_used.data, form.max_power_used.data)
-				amount_paid = random.randint(form.min_amount_paid.data, form.max_amount_paid.data)
-				
-				location = ""
-				if form.location.data is not "":
-					location = form.location.data
-				else:
-					location = random.choice(string.ascii_letters)
-
-				port = random.randint(form.min_port.data, form.max_port.data)
-				increment_size = random.randint(form.min_increment_size.data, form.max_increment_size.data)
-				increments = math.ceil(duration / increment_size)
-
-				session = Session(duration=duration, power_used=power_used, amount_paid=amount_paid,
-									location=location, port=port, increment_size=increment_size,
-									increments=increments)
-
-				db.session.add(session)
-			db.session.commit()
-
-			flash('Sessions Added!', 'success')
-			return redirect(url_for('kiosk_mode.add_rand_session'))
-
-	return render_template('kiosk_mode/add_rand_session.html', title='Random', form=form)
