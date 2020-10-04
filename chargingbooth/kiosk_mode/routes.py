79cacb1d5c0e20a8da98461559c92a562b8d1abf
Christian Rodriguez <christianrodriguez795@yahoo.com>
2020-10-03 23:26:34 -0700

    Added grab server settings for kisok mode

--- chargingbooth/kiosk_mode/routes.py
+++ chargingbooth/kiosk_mode/routes.py
@@ -8,7 +8,7 @@ from chargingbooth import db, bcrypt, current_sessions, service_ip
 from chargingbooth.models import Session, Settings
 from chargingbooth.kiosk_mode.utils import (start_route, get_offset_dates_initiated, get_offset_dates_end,
 											split_seconds, is_registered)
-from chargingbooth.models import PFI
+from chargingbooth.models import PFI, Device_ID
 import requests
 
 kiosk_mode = Blueprint('kiosk_mode', __name__)
@@ -25,21 +25,32 @@ def home():
 	pic_files = PFI()
 
 	# TODO: make settings obtainable form service
-	# Grab settings from site
-	# try:
-	# 	payload = requests.get(service_ip + '/device/')
-	# except:
 
-	setting = Settings.query.first()
+	devi_id_number = Device_ID.query.first().id_number
+
+	# Grab settings from site
+	try:
+		payload = requests.get(service_ip + '/device/get_settings/' + devi_id_number)
+	except:
+		flash("Unable to Connect to Server!", "danger")
+		return redirect(url_for('register.error'))
+
+	print(payload.json())
+
+	# setting = Settings.query.first()
+	setting = payload.json()
 
 	sessions = current_sessions.local_sessions.values()
-	date_strings = get_offset_dates_initiated(sessions=sessions, time_offset=Settings.query.first().time_offset)
-	date_end_str = get_offset_dates_end(sessions=sessions, time_offset=Settings.query.first().time_offset)
+	# date_strings = get_offset_dates_initiated(sessions=sessions, time_offset=Settings.query.first().time_offset)
+	# date_end_str = get_offset_dates_end(sessions=sessions, time_offset=Settings.query.first().time_offset)
+	date_strings = get_offset_dates_initiated(sessions=sessions, time_offset=setting["time_offset"])
+	date_end_str = get_offset_dates_end(sessions=sessions, time_offset=setting["time_offset"])
 
 	sessions_and_dates = zip(sessions, date_strings, date_end_str)
 
 	# Get the total hours, minutes, seconds for the session time
-	hours, minutes, seconds = split_seconds(setting.charge_time)
+	# hours, minutes, seconds = split_seconds(setting.charge_time)
+	hours, minutes, seconds = split_seconds(setting["charge_time"])
 
 	# return render_template('kiosk_mode/home.html', title='Kiosk Mode', 
 	# 						current_sessions=current_sessions,
@@ -76,10 +87,21 @@ def make_session():
 
 	# Only make a session if there is no session currently available
 	if not current_sessions.has_sessions():
-		setting = Settings.query.first()
+		devi_id_number = Device_ID.query.first().id_number
+		
+		try:
+			payload = requests.get(service_ip + '/device/get_settings/' + devi_id_number)
+		except:
+			flash("Unable to Connect to Server!", "danger")
+			return redirect(url_for('register.error'))
 
-		current_sessions.add_session(amount_paid=setting.price, location=setting.location,
-										port="", increment_size=setting.charge_time, increments=1)
+		# setting = Settings.query.first()
+		setting = payload.json()
+
+		# current_sessions.add_session(amount_paid=setting.price, location=setting.location,
+		# 								port="", increment_size=setting.charge_time, increments=1)
+		current_sessions.add_session(amount_paid=setting["price"], location=setting["location"],
+										port="", increment_size=setting["charge_time"], increments=1)
 		flash('Session Added Successfully! You may start charging now.')
 
 	return redirect(url_for('kiosk_mode.home'))
