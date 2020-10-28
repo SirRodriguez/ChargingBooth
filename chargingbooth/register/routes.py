c4340e8f7fb0435c299b167f1653158df001f8d6
Christian Rodriguez <christianrodriguez795@yahoo.com>
2020-10-27 21:13:32 -0700

    Added a verification for register device

--- chargingbooth/register/routes.py
+++ chargingbooth/register/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, json, flash
 from chargingbooth import db, service_ip
 from chargingbooth.models import Device_ID
+from chargingbooth.register.forms import LoginForm
 import requests
 
 register = Blueprint('register', __name__)
@@ -11,20 +12,35 @@ def home():
 	return render_template("register/home.html", title="Register Device")
 
 # Device is registered
-@register.route("/register/register_device")
+@register.route("/register/register_device", methods=['GET', 'POST'])
 def register_device():
-	# Register device here
-	try:
-		payload = requests.get(service_ip + '/device/register')		
-	except:
-		flash("Unable to Connect to Server!", "danger")
-		return redirect(url_for('error.register'))
-	
-	pl_json = payload.json()
-	device_id = pl_json["device_id"]
-	id = pl_json["id"]
+	form = LoginForm()
 
-	Device_ID.query.first().id_number = device_id
-	db.session.commit()
+	if form.validate_on_submit():
+		json_send = {}
+		json_send["username"] = form.username.data
+		json_send["password"] = form.password.data
 
-	return render_template("register/registered.html", title="Registered", id=id)
+		# Register device here
+		try:
+			payload = requests.get(service_ip + '/device/register', json=json_send)		
+		except:
+			flash("Unable to Connect to Server!", "danger")
+			return redirect(url_for('error.register'))
+
+		# Verify user
+		if payload.status_code == 401:
+			flash("Username or password not correct", 'danger')
+
+		else:
+			# Verified
+			pl_json = payload.json()
+			device_id = pl_json["device_id"]
+			id = pl_json["id"]
+
+			Device_ID.query.first().id_number = device_id
+			db.session.commit()
+
+			return render_template("register/registered.html", title="Registered", id=id)
+
+	return render_template("register/verify.html", title="Verify", form=form)
