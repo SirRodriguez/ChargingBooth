3dba0baf9bdd6713b5bf181458426406a2951926
Christian Rodriguez <christianrodriguez795@yahoo.com>
2020-11-24 22:15:07 -0800

    removed bad syntax for python

--- chargingbooth/models.py
+++ chargingbooth/models.py
@@ -28,9 +28,6 @@ class User(db.Model, UserMixin):
 	email = db.Column(db.String(120), unique=True, nullable=False)
 	password = db.Column(db.String(60), nullable=False)
 
-	def __repr__(self):
-		return f"User('{self.username}', '{self.email}')"
-
 	def get_reset_token(self, expires_sec=1800):
 		s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
 		return s.dumps({'user_id': self.id}).decode('utf-8')
