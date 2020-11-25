7301cad23f175e975e3ecba5912630e43f4c7906
Christian Rodriguez <christianrodriguez795@yahoo.com>
2020-11-24 22:37:26 -0800

    Chnaged the string syntax

--- chargingbooth/system_admin/account/utils.py
+++ chargingbooth/system_admin/account/utils.py
@@ -6,17 +6,19 @@ def send_reset_email(email, user, logged_in=False):
 	token = user.get_reset_token()
 	msg = Message('Password Reset Request', sender='noreply@demo.com', recipients=[email])
 	if logged_in:
-		msg.body = f'''To reset your password, visit the following link:
+# 		msg.body = f'''To reset your password, visit the following link:
 
-{url_for('system_admin_account.change_token', token=token, _external=True)}
+# {url_for('system_admin_account.change_token', token=token, _external=True)}
 
-If you did not make this request then simply ignore this email and no change will be made.
-'''
+# If you did not make this request then simply ignore this email and no change will be made.
+# '''
+		msg.body = "To reset your password, visit the following link:\n\n" + url_for('system_admin_account.change_token', token=token, _external=True) + "\n\nIf you did not make this request then simply ignore this email and no change will be made."
 	else:
-		msg.body = f'''To reset your password, visit the following link:
+# 		msg.body = f'''To reset your password, visit the following link:
 
-{url_for('system_admin_account.reset_token', token=token, _external=True)}
+# {url_for('system_admin_account.reset_token', token=token, _external=True)}
 
-If you did not make this request then simply ignore this email and no change will be made.
-'''
+# If you did not make this request then simply ignore this email and no change will be made.
+# '''
+		msg.body = "To reset your password, visit the following link:\n\n" + url_for('system_admin_account.change_token', token=token, _external=True) + "\n\nIf you did not make this request then simply ignore this email and no change will be made."
 	mail.send(msg)
