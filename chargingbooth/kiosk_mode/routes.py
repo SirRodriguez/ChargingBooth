c7f53bd60e64714dccb74d177bdb78a20cace125
Christianrodriguez795@yahoo.com <christianrodriguez795@yahoo.com>
2021-06-09 23:00:50 -0700

    Remove home fast route

--- chargingbooth/kiosk_mode/routes.py
+++ chargingbooth/kiosk_mode/routes.py
@@ -6,7 +6,7 @@ import os
 from datetime import datetime
 from flask import render_template, Blueprint, redirect, url_for, flash, request
 from flask_login import current_user, logout_user
-from chargingbooth import db, bcrypt, current_sessions, service_ip, settings_cache
+from chargingbooth import db, bcrypt, current_sessions, service_ip
 from chargingbooth.kiosk_mode.utils import (start_route, get_offset_dates_initiated, get_offset_dates_end,
 											split_seconds, is_registered)
 from chargingbooth.models import PFI, Device_ID
@@ -14,58 +14,6 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 kiosk_mode = Blueprint('kiosk_mode', __name__)
-
-## Test routes
-
-
-@kiosk_mode.route("/setup")
-def setup():
-	return render_template("paypal/setup.html")
-
-@kiosk_mode.route("/paypal_test")
-def paypal():
-	# Authenticate token
-	try:
-		# url
-		url = "https://api-m.sandbox.paypal.com/v1/oauth2/token"
-		# header
-		headers = {
-			"Accept": "application/json",
-			"Accept-Language": "en_US"
-		}
-		# Basic Auth
-		Client_ID = os.environ.get('CLIENT_ID')
-		SECRET = os.environ.get('SECRET')
-		auth = HTTPBasicAuth(Client_ID, SECRET)
-		# Data
-		data = {
-			"grant_type": "client_credentials"
-		}
-		# POST
-		payload = requests.post(url, headers=headers, auth=auth, data=data)
-		access_token = payload.json()['access_token']
-	except Exception as e:
-		print("In Exception")
-		print(e)
-
-	# generate token
-	try:
-		url = "https://api-m.sandbox.paypal.com/v1/identity/generate-token"
-		headers = {
-			"Content-Type": "application/json",
-			"Authorization": "Bearer " + access_token,
-			"Accept-Language": "en_US"
-		}
-		payload = requests.post(url, headers=headers)
-		client_token = payload.json()["client_token"]
-	except Exception as e:
-		print("In Exception")
-		print(e)
-
-
-	return render_template("paypal/test.html", client_ID=os.environ.get('CLIENT_ID'), client_token=client_token)
-
-## End test routes
 
 @kiosk_mode.route("/kiosk_mode", methods=['GET', 'POST'])
 def home():
@@ -127,47 +75,47 @@ def home():
 
 # This is used when a user creats a session. It is much faster as the settings and image
 # count are cached and passed through. It avoids the delayed timer showing on the UI.
-@kiosk_mode.route("/kiosk_mode/<int:image_count>", methods=['GET', 'POST'])
-def home_fast(image_count):
-	start_route()
+# @kiosk_mode.route("/kiosk_mode/<int:image_count>", methods=['GET', 'POST'])
+# def home_fast(image_count):
+# 	start_route()
 
-	devi_id_number = Device_ID.query.first().id_number
+# 	devi_id_number = Device_ID.query.first().id_number
 
-	sessions = current_sessions.local_sessions.values()
+# 	sessions = current_sessions.local_sessions.values()
 
-	# Grab the settings from cache
-	# It is put in a map because the html page access it that way
-	setting = {}
-	setting["toggle_pay"] = settings_cache.get_toggle_pay()
-	setting["price"] = settings_cache.get_price()
-	setting["charge_time"] = settings_cache.get_charge_time()
-	setting["time_offset"] = settings_cache.get_time_offset()
-	setting["location"] = settings_cache.get_location()
-	setting["aspect_ratio_width"] = settings_cache.get_aspect_ratio_width()
-	setting["aspect_ratio_height"] = settings_cache.get_aspect_ratio_height()
+# 	# Grab the settings from cache
+# 	# It is put in a map because the html page access it that way
+# 	setting = {}
+# 	setting["toggle_pay"] = settings_cache.get_toggle_pay()
+# 	setting["price"] = settings_cache.get_price()
+# 	setting["charge_time"] = settings_cache.get_charge_time()
+# 	setting["time_offset"] = settings_cache.get_time_offset()
+# 	setting["location"] = settings_cache.get_location()
+# 	setting["aspect_ratio_width"] = settings_cache.get_aspect_ratio_width()
+# 	setting["aspect_ratio_height"] = settings_cache.get_aspect_ratio_height()
 
-	date_strings = get_offset_dates_initiated(sessions=sessions, time_offset=setting["time_offset"])
-	date_end_str = get_offset_dates_end(sessions=sessions, time_offset=setting["time_offset"])
+# 	date_strings = get_offset_dates_initiated(sessions=sessions, time_offset=setting["time_offset"])
+# 	date_end_str = get_offset_dates_end(sessions=sessions, time_offset=setting["time_offset"])
 
-	sessions_and_dates = zip(sessions, date_strings, date_end_str)
+# 	sessions_and_dates = zip(sessions, date_strings, date_end_str)
 
-	# Get the total hours, minutes, seconds for the session time
-	# hours, minutes, seconds = split_seconds(setting.charge_time)
-	hours, minutes, seconds = split_seconds(setting["charge_time"])
+# 	# Get the total hours, minutes, seconds for the session time
+# 	# hours, minutes, seconds = split_seconds(setting.charge_time)
+# 	hours, minutes, seconds = split_seconds(setting["charge_time"])
 
-	# Random hex, used in the url of the images in order to reset the cache of the browser
-	random_hex = secrets.token_hex(8)
+# 	# Random hex, used in the url of the images in order to reset the cache of the browser
+# 	random_hex = secrets.token_hex(8)
 
-	return render_template('kiosk_mode/homeV2.html', 
-							title='Kiosk Mode', 
-							current_sessions=current_sessions,
-							sessions_and_dates=sessions_and_dates,
-							service_ip=service_ip,
-							devi_id_number=devi_id_number,
-							img_count=image_count,
-							random_hex=random_hex,
-							setting=setting, 
-							hours=hours, minutes=minutes, seconds=seconds)
+# 	return render_template('kiosk_mode/homeV2.html', 
+# 							title='Kiosk Mode', 
+# 							current_sessions=current_sessions,
+# 							sessions_and_dates=sessions_and_dates,
+# 							service_ip=service_ip,
+# 							devi_id_number=devi_id_number,
+# 							img_count=image_count,
+# 							random_hex=random_hex,
+# 							setting=setting, 
+# 							hours=hours, minutes=minutes, seconds=seconds)
 
 
 @kiosk_mode.route("/kiosk_mode/confirm_payment")
@@ -178,6 +126,18 @@ def confirm_payment():
 	# Once they are passed, They will be redirected to make a session
 	# If they fail, then they are redirected back to home
 	# For now, it passes only
+
+	devi_id_number = Device_ID.query.first().id_number
+
+	# /device/get_settings/<string:id_number>	
+	# Grab the number of images the service has, also settings
+	try:
+		payload = requests.get(service_ip + '/device/get_settings/' + devi_id_number)
+	except:
+		flash("Unable to Connect to Server!", "danger")
+		return redirect(url_for('error.register'))
+
+	
 
 	# If there is already a session, Just add time to it
 	# Else, Make a new session
@@ -208,16 +168,16 @@ def make_session():
 		img_count = pl_json["image_count"]
 		setting = pl_json["settings"]
 
-		# Place Settings in the cache
-		settings_cache.set_values(
-			toggle_pay=setting["toggle_pay"],
-			price=setting["price"],
-			charge_time=setting["charge_time"],
-			time_offset=setting["time_offset"],
-			location=setting["location"],
-			aspect_ratio_width=setting["aspect_ratio_width"],
-			aspect_ratio_height=setting["aspect_ratio_height"]
-		)
+		# # Place Settings in the cache
+		# settings_cache.set_values(
+		# 	toggle_pay=setting["toggle_pay"],
+		# 	price=setting["price"],
+		# 	charge_time=setting["charge_time"],
+		# 	time_offset=setting["time_offset"],
+		# 	location=setting["location"],
+		# 	aspect_ratio_width=setting["aspect_ratio_width"],
+		# 	aspect_ratio_height=setting["aspect_ratio_height"]
+		# )
 
 		current_sessions.add_session(amount_paid=setting["price"], location=setting["location"],
 										port="", increment_size=setting["charge_time"], increments=1)
