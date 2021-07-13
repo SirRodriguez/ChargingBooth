51d43d27770b3fe5172e327e9b9322b8d863cebe
Christian Rodriguez <christianrodriguez795@yahoo.com>
2021-07-12 22:59:12 -0700

    Added protection to the device from other devices on the network

--- chargingbooth/main/routes.py
+++ chargingbooth/main/routes.py
@@ -1,7 +1,15 @@
-from flask import Blueprint, url_for, redirect, flash
+from flask import Blueprint, url_for, redirect, flash, request, abort
 from chargingbooth.main.utils import start_route, is_registered
 
 main = Blueprint('main', __name__)
+
+# This is the function that will be called so that no other device
+# On the network can access the routes
+@main.before_app_request
+def ip_check():
+	# Check if it is not localhost
+	if request.remote_addr != "127.0.0.1":
+		abort(403)
 
 @main.route("/")
 @main.route("/home")
